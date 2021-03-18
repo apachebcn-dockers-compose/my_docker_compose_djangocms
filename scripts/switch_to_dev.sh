7962e632d3ea5c1c523cf3c480be4401c4fa112b
root <root@vmi400843.contaboserver.net>
2021-03-18 21:17:47 +0100

    scripts y nginx

--- scripts/switch_to_dev.sh
+++ scripts/switch_to_dev.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-cd ..
-rm -r ./.env
-cp .env-dev .env
+rm -r ../.env
+cp ../.env-dev ../.env
+
+rm -r ../docker-compose.yml
+cp ../docker-compose-dev.yml ../docker-compose.yml
+
+rm -f ../bin/scripts/start.sh
+cp  ../bin/scripts/start-dev.sh ../bin/scripts/start.sh
+
+rm -f ../IS-IN-MODE-PROD
+touch ../IS-IN-MODE-DEV
 
 cd ..
-rm -r ./docker-compose.yml
-cp docker-compose-dev.yml docker-compose.yml
-
-rm -f ./bin/scripts/start.sh
-cp  ./bin/scripts/start-dev.sh ./bin/scripts/start.sh
-
-rm -f ./IS-IN-MODE-PROD
-touch ./IS-IN-MODE-DEV
-
 docker-compose up --build -d
--- scripts/switch_to_prod.sh
+++ scripts/switch_to_prod.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-cd ..
-rm -r ./.env
-cp .env-prod .env
+rm -r ../.env
+cp ../.env-prod ../.env
+
+rm -f ../docker-compose.yml
+cp ../docker-compose-prod.yml ../docker-compose.yml
+
+rm -f ../bin/scripts/start.sh
+cp  ../bin/scripts/start-prod.sh ../bin/scripts/start.sh
+
+rm -f ../IS-IN-MODE-DEV
+touch ../IS-IN-MODE-PROD
 
 cd ..
-rm -f ./docker-compose.yml
-cp docker-compose-prod.yml docker-compose.yml
-
-rm -f ./bin/scripts/start.sh
-cp  ./bin/scripts/start-prod.sh ./bin/scripts/start.sh
-
-rm -f ./IS-IN-MODE-DEV
-touch ./IS-IN-MODE-PROD
-
 docker-compose up --build -d
